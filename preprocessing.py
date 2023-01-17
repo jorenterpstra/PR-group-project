4a4d3b32a11c29872261d7b3308965bac412d396
jorenterptsra <jorenterpstra@gmail.com>
2023-01-17 11:42:06 +0100

    preprocessing with subwords

--- preprocessing.py
+++ preprocessing.py
@@ -3,10 +3,11 @@ import regex as re
 import numpy as np
 import pandas as pd
 
-from keras.preprocessing.text import Tokenizer
-from keras.utils import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
-from keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
+from tokenizers import BertWordPieceTokenizer
 
 
 def preprocess(text):
@@ -26,47 +27,51 @@ def preprocess(text):
     return text
 
 
-def encode_text_and_labels(df, min_occurrence=0):
-    t = Tokenizer()
-    t.fit_on_texts(df['text'])
+def encode_text_and_labels(df, max_num_words, pre_or_post='post', subword=False):
+    # create a tokenizer
+    if subword:
+        t = BertWordPieceTokenizer(
+            clean_text=True,
+            handle_chinese_chars=False,
+            strip_accents=False,
+            lowercase=True
+        )
 
-    if min_occurrence > 0:
-        # keep only words that appear more than min_occurence times
-        word_docs = {word: freq for word, freq in t.word_docs.items() if freq > min_occurrence}
-
-        # create new tokenizer
-        t_filtered = Tokenizer()
-
-        # fit t_filtered
-        t_filtered.word_index = {word: i + 1 for i, (word, freq) in enumerate(word_docs.items())}
-        t_filtered.word_counts = word_docs
+        t.train_from_iterator(df['text'])
+        vocab_size = t.get_vocab_size()
+        # integer encode the documents
+        encoded_list = t.encode_batch(df['text'])
+        encoded_docs = [x.ids for x in encoded_list]
+        # pad documents to be as long as the longest sequence in the dataset
+        max_length = max([len(x) for x in encoded_docs])
     else:
-        t_filtered = t
+        t = Tokenizer(num_words=max_num_words, oov_token='<unk>')
+        t.fit_on_texts(df['text'])
+        vocab_size = len(t.word_index) + 1
+        # integer encode the documents
+        encoded_docs = t.texts_to_sequences(df['text'])
+        # pad documents to be as long as the longest sequence in the dataset
+        max_length = df['text'].apply(lambda x: len(x.split(' '))).max()
 
-    vocab_size = len(t_filtered.word_index) + 1
-    # integer encode the documents
-    encoded_docs = t_filtered.texts_to_sequences(df['text'])
-    # pad documents to be as long as the longest sequence in the dataset
-    max_length = df['text'].apply(lambda x: len(x.split(' '))).max()
-    padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+    padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding=pre_or_post)
 
     # integer encode
     label_encoder = LabelEncoder()
     integer_encoded = label_encoder.fit_transform(df['artist'])
     # binary encode
     onehot_encoded = to_categorical(integer_encoded)
-    return padded_docs, onehot_encoded, vocab_size, max_length
+    return padded_docs, onehot_encoded, vocab_size, max_length, t
 
-def load_and_preprocess_data(path, in_colab=False):
+
+def load_and_preprocess_data(path, max_num_words=None, pre_or_post='post', subword=False):
     """
-    Load the data and preprocess it, expect runtime of 20 seconds.
+    Load the data and preprocess it
     :param path: path to the data
     :return: preprocessed data in the form of a pandas dataframe. The first item returned is the data,
     the second is the labels, the third is the vocabulary size, and the fourth is the maximum length of a sequence
     """
     df = pd.read_csv(path)
-      
-    # remove artist with fewer than 30 songs
+
     df = df.groupby('artist').filter(lambda x: len(x) > 100)
 
     df['text'] = df['text'].apply(preprocess)
@@ -78,4 +83,4 @@ def load_and_preprocess_data(path, in_colab=False):
     df = df[no_covers]
 
     # prepare text data for a recurrent network
-    return encode_text_and_labels(df)
+    return encode_text_and_labels(df, max_num_words, pre_or_post, subword)
